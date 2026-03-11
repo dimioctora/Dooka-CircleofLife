06be4c5fbb4144333797394630648f71eb70c1f2
dimioctora <dimioctora@gmail.com>
2026-03-11 10:17:18 +0700

    Deployment: Set base path for production build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/circle-of-life/',
   server: {
     port: 5188,
     strictPort: true,
