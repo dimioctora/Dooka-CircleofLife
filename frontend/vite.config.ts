2f8df677f1eb034fb4ec037a8a63281e7b930680
dimioctora <dimioctora@gmail.com>
2026-03-11 10:33:55 +0700

    Deployment: Move graph to /circle-graph to prevent collision with Laravel route

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/circle-of-life/',
+  base: '/circle-graph/',
   server: {
     port: 5188,
     strictPort: true,
